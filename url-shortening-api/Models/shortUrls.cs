67bdb1801b877a76e291c82b3dd2e7fc8353a038
Raul Hernandez <rehcgt@outlook.com>
2024-06-28 15:16:44 -0600

    Added API functionality and positioning top divs

---
+++ url-shortening-api/Models/shortUrls.cs
@@ -0,0 +1,7 @@
+﻿public class shortUrls
+{
+    public int Id { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public string shortUrl { get; set; } = string.Empty;
+    public string buttonText { get; set; } = "Copy";
+}
--- url-shortening-api/Program.cs
+++ url-shortening-api/Program.cs
@@ -1,3 +1,4 @@
+using BlazorToastify;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using url_shortening_api;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ClipboardService>();
+builder.Services.AddSingleton<IToastService, ToastService>();
 
 await builder.Build().RunAsync();
