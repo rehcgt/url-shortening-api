e3309dd86c99a38097e8eb15307b2821cc3b7e88
Raul Hernandez <rehcgt@outlook.com>
2024-07-08 17:55:57 -0600

    fixing mobile layout

--- url-shortening-api/Models/shortUrls.cs
+++ url-shortening-api/Models/shortUrls.cs
@@ -4,4 +4,6 @@
     public string Url { get; set; } = string.Empty;
     public string shortUrl { get; set; } = string.Empty;
     public string buttonText { get; set; } = "Copy";
+    public string additionalButtonClass { get; set; } = "bg-cyan";
+    public bool isDisabled { get; set; } = false;
 }
